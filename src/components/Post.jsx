8b019fc68646544c56d5a23503cad6d915300781
Paulo Abrantes <pauloabrantes@MacBook-Air-de-Paulo.local>
2023-03-18 13:45:48 -0300

    :tada: initial commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,19 @@
+import styles from "./Style.css?inline";
+
+
+export function Post() {
+  
+
+  return (
+    <div className="Container">
+       <h3>the <span>horror game</span> of the year</h3>
+       <h1>ENDLESS NIGHTMARE</h1>
+       <p>You need to find the way out or your night will never end!</p>
+       <a href="#">READ MORE</a>
+       <a href="#" class="btn-primary">PLAY NOW</a>
+    </div>
+   
+  )
+}
+
+
